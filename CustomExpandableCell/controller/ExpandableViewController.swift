3378af74ef65dcb5a5a3fa07e2531d5a95a20c2e
Tanin Rojanapiansatith <sp_land@hotmail.com>
2018-06-04 14:48:24 +0100

    Add mute button and reveal it with the swipe

--- CustomExpandableCell/controller/ExpandableViewController.swift
+++ CustomExpandableCell/controller/ExpandableViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ExpandableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var expandTable: UITableView!
+//    var header: UIView!
+    var offset:CGFloat = 0
     
     var expandData = [NSMutableDictionary]()
+    var headerTitles = ["lbl 1", "lbl 2", "lbl 3", "lbl 4", "lbl 5", "lbl 6"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,19 +82,18 @@ class ExpandableViewController: UIViewController, UITableViewDelegate, UITableVi
 
         let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "ExpandableCell") as! ExpandableCellCell
         
-        headerView.alarmGroupNameLbl.text = "Name lbl"
+        headerView.alarmGroupNameLbl.text = headerTitles[section]
         headerView.alarmGroupTimeLbl.text = "Time lbl works"
 //        headerView.expandableCellCellDelegate = self
         headerView.backgroundColor = UIColor.blue
-//        headerView.contentView.backgroundColor = UIColor.brown
         
         headerView.tag = section + 100
         
         let tapgesture = UITapGestureRecognizer(target: self , action: #selector(self.sectionTapped(_:)))
         headerView.addGestureRecognizer(tapgesture)
         
-        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
-        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(sender:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(sender:)))
         
 //        leftSwipe.setValue(section, forKey: "sectionKey")
 //        rightSwipe.setValue(section, forKey: "sectionKey")
@@ -99,8 +101,14 @@ class ExpandableViewController: UIViewController, UITableViewDelegate, UITableVi
 //        leftSwipe.setValue(section, forKeyPath: "sectionKey")
 //        rightSwipe.setValue(section, forKeyPath: "sectionKey")
         
-        leftSwipe.setValue(section, forUndefinedKey: "sectionKey")
-        rightSwipe.setValue(section, forUndefinedKey: "sectionKey")
+//        leftSwipe.setValue(section, forUndefinedKey: "sectionKey")
+//        rightSwipe.setValue(section, forUndefinedKey: "sectionKey")
+        
+//        leftSwipe.setValuesForKeys(["sectionKey": section])
+//        rightSwipe.setValuesForKeys(["sectionKey": section])
+        
+//        leftSwipe.setValue(section, forKeyPath: "sectionKey")
+//        rightSwipe.setValue(section, forKeyPath: "sectionKey")
         
         leftSwipe.direction = .left
         rightSwipe.direction = .right
@@ -108,30 +116,69 @@ class ExpandableViewController: UIViewController, UITableViewDelegate, UITableVi
         headerView.addGestureRecognizer(leftSwipe)
         headerView.addGestureRecognizer(rightSwipe)
         
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+//        headerView.addGestureRecognizer(panGesture)
+        
+        let buttonWidth: CGFloat = 140
+        let buttonHeight: CGFloat = 122
+        let button = UIButton(frame: CGRect(x: headerView.frame.origin.x - 140, y: headerView.frame.origin.y, width: buttonWidth, height: buttonHeight))
+        button.layer.cornerRadius = 10
+        button.backgroundColor = UIColor.HomeScreen.Status.Red!
+        button.setTitle("Mute", for: .normal)
+        //        button.titleLabel?.font = UIFont(name: "SFUIDisplay-Bold", size: 14)
+        button.addTarget(self, action: #selector(self.newDataAvailableBtnTapped), for: .touchUpInside)
+//        button.layer.shadowRadius = 2
+//        button.layer.shadowColor = UIColor.black.cgColor
+//        button.layer.shadowOffset = CGSize(width: 2, height: 2)
+//        button.layer.shadowOpacity = 0.3
+        
+        headerView.addSubview(button)
+        headerView.clipsToBounds = false
+        
         return headerView
 
     }
     
-    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
-        
-        let headerCell = expandTable.headerView(forSection: sender.value(forKey: "sectionKey") as! Int)!
-        
-        if (sender.direction == .left) {
-            print("Swipe Left")
-            let labelPosition = CGPoint(x: headerCell.frame.origin.x - 150.0, y: headerCell.frame.origin.y)
-            UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
-                headerCell.frame = CGRect(x: labelPosition.x, y: labelPosition.y, width: headerCell.frame.size.width, height: headerCell.frame.size.height)
-            }, completion: nil)
-            
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y)
         }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @objc func handleSwipes(sender:UISwipeGestureRecognizer) {
         
-        if (sender.direction == .right) {
-            print("Swipe Right")
-            let labelPosition = CGPoint(x: headerCell.frame.origin.x + 150.0, y: headerCell.frame.origin.y)
+//        let sectionNum = sender.value(forKey: "sectionKey") as! Int
+        let headerCell = expandTable.headerView(forSection: 0)!
+        
+//        self.expandTable.beginUpdates()
+//        self.expandTable.deleteSections([0], with: .right)
+//        self.expandTable.insertSections([2], with: .left)
+//        self.expandTable.endUpdates()
+        
+        if (sender.direction == .left && headerCell.frame.origin.x != 0) {
+
+            print("Swipe Left")
+            let labelPosition = CGPoint(x: headerCell.frame.origin.x - 140.0, y: headerCell.frame.origin.y)
             UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
+                
                 headerCell.frame = CGRect(x: labelPosition.x, y: labelPosition.y, width: headerCell.frame.size.width, height: headerCell.frame.size.height)
+                
             }, completion: nil)
-           
+
+        }
+
+        if (sender.direction == .right && headerCell.frame.origin.x == 0) {
+            print("Swipe Right")
+            let labelPosition = CGPoint(x: headerCell.frame.origin.x + 140.0, y: headerCell.frame.origin.y)
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
+                
+                headerCell.frame = CGRect(x: labelPosition.x, y: labelPosition.y, width: headerCell.frame.size.width, height: headerCell.frame.size.height)
+                
+            }, completion: nil)
+
         }
     }
     
@@ -147,6 +194,18 @@ class ExpandableViewController: UIViewController, UITableViewDelegate, UITableVi
         
     }
     
+    // MARK: Action methods
+    @objc func newDataAvailableBtnTapped(sender: UIButton!) {
+        print("Button tapped")
+        
+        let top = CGAffineTransform(translationX: 0, y: -43)
+        UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
+            // Add the transformation in this block
+            sender.transform = top
+        }, completion: nil)
+        
+    }
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 122
     }
@@ -154,27 +213,5 @@ class ExpandableViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
-            
-        }
-        
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
     
 }
-
-//extension ExpandableViewController: ExpandableCellNavBtnDelegator {
-//    
-//    func callSegueFromHomeCell(in section: Int) {
-//        print("It works!")
-//    }
-//  
-//}
